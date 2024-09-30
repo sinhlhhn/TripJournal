8f85907975b131914237781ada92cd15a5f2dfe3
sinhlh <sinhlh>
2024-09-30 21:47:40 +0700

    add coding keys for Token model

--- TripJournal/Models/Models.swift
+++ TripJournal/Models/Models.swift
@@ -5,6 +5,11 @@ import MapKit
 struct Token: Codable {
     let accessToken: String
     let tokenType: String
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case tokenType = "token_type"
+    }
 }
 
 /// Represents a trip.
